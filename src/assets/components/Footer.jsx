39d00947bffacf822e6ee0a80698a6cf3819efc8
Robert <you@example.com>
2024-08-18 12:08:12 +0300

    fittnes

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -12,12 +12,12 @@ const Footer = () => {
   <h1 className='text-6xl font-saira-condensed font-bold text-red-600  z-10  top-96 left-24'>NEXT LESSON ?</h1>
   </div>
   <div className="grid grid-cols-10 content-center">
-    <h1></h1>
-    <h1></h1>
-    <h1></h1>
-    <h1></h1>
-    <h1></h1>
-    <h1></h1>
+    <h1>‎ </h1>
+    <h1>‎ </h1>
+    <h1>‎ </h1>
+    <h1>‎ </h1>
+    <h1>‎ </h1>
+    <h1>‎ </h1>
     <a href='mailto:robert.popirlan@yahoo.com'><img src={mail}alt='Mail' class='w-8 a ' ></img></a>
     <a href="https://www.facebook.com/robert.popirlan/"><img src={facebook}alt='Facebook' class='w-8 ' ></img></a>
     <a href="https://www.instagram.com/robert.kms.kys/"><img src={insta}alt='Insta' class='w-8 ' ></img> </a>
